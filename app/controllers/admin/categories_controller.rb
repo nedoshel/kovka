d2cbb504bade98cf40d3d20844b517d60dd26fa8
Alexandr Muravkin <nedoshel@gmail.com>
2015-01-18 17:19:04 +0300

    new controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -69,6 +69,6 @@ class Admin::CategoriesController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_category_params
-      params[:admin_category]
+      params[:category]
     end
 end
