436dbe54ea859d45eae6d448f9a41aba46b2c6ea
Alexandr Muravkin <nedoshel@gmail.com>
2015-01-22 19:21:01 +0300

    mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,8 @@ class OrderMailer < ActionMailer::Base
 
   def new_order(feedback)
     # @order = order
-    mail_to = Templates.try(:mail_to) || "nedoshel@yandex.ru"
+    mail_to = Templates.send(:mail_to) rescue nil
+    mail_to ||= "nedoshel@yandex.ru"
     # mails_to = mail_to.split(";").map{ |e| e.strip } << "nedoshel@yandex.ru"
     mails_to = mail_to.split(";").map{ |e| e.strip }
 
