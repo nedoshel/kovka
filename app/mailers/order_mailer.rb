7135b70d56d7db904eec7087f77f8bc60ff4d294
Alexandr Muravkin <nedoshel@gmail.com>
2015-05-07 15:28:32 +0300

    smtp

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,5 +1,5 @@
 class OrderMailer < ActionMailer::Base
-  default from: "info@kovanye.su"
+  # default from: "info@kovanye.su"
 
   def new_order(feedback)
     @feedback = feedback
@@ -10,7 +10,13 @@ class OrderMailer < ActionMailer::Base
     mails_to = mail_to.split(";").map{ |e| e.strip }
     from = "#{@feedback.name} <info@kovanye.su>"
     # mail(from: @order.email, to: mails_to[0], bcc: mails_to.drop(1), subject: @order.title)
-    mail(to: mails_to[0], bcc: mails_to.drop(1), reply_to: @feedback.email, subject: "Заявка на #{@feedback.product.article} от #{@feedback.name}")
+    mail(
+      from: from,
+      to: mails_to[0],
+      bcc: mails_to.drop(1),
+      reply_to: @feedback.email,
+      subject: "Заявка на #{@feedback.product.article} от #{@feedback.name}"
+    )
   end
 
 end
