fa01398f9a617e8561eaa42b1466cbebdc4fec10
Alexandr Muravkin <nedoshel@gmail.com>
2015-05-07 15:25:08 +0300

    smtp

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,7 +8,7 @@ class OrderMailer < ActionMailer::Base
     mail_to ||= "nedoshel@yandex.ru"
     # mails_to = mail_to.split(";").map{ |e| e.strip } << "nedoshel@yandex.ru"
     mails_to = mail_to.split(";").map{ |e| e.strip }
-
+    from = "#{@feedback.name} <info@kovanye.su>"
     # mail(from: @order.email, to: mails_to[0], bcc: mails_to.drop(1), subject: @order.title)
     mail(to: mails_to[0], bcc: mails_to.drop(1), reply_to: @feedback.email, subject: "Заявка на #{@feedback.product.article} от #{@feedback.name}")
   end
